f8ab6bb261c004f345d08e7b9298b05c2afc859d
srajeev1992 <46666031+srajeev1992@users.noreply.github.com>
2020-11-15 09:19:16 -0800

    Update gameSelect.js

--- public/js/scene/gameSelect.js
+++ public/js/scene/gameSelect.js
@@ -14,7 +14,7 @@ export default class SelectScene extends Phaser.Scene {
 		// Change these buttons to their respective maps later on (TODO LATER)
 		this.createForestBtn('forestBtn', 'forestBtnHover', 'Forest Level', 'Forest', 3);
 		this.createIslandBtn('beachBtn', 'beachBtnHover', 'Island Level', 'Island', 2);
-		this.createVillageBtn('villageBtn', 'villageBtnHover', 'Village Level', 'Game', 1);
+		this.createVillageBtn('villageBtn', 'villageBtnHover', 'Village Level', 'Village', 1);
 		this.createBackBtn('redBtn', 'redBtnHover', 'Back', 'Title', 0);
 
 	}
